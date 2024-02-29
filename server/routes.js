6f9165994355fa6a0d623b7e768091884a58d1d7
Roman Tkachuk <tkachukrr@gmail.com>
2024-02-29 14:45:38 +0100

    Implemented ability to control requests quantity per second

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const REQUEST_LIMIT = 50;
+const REQUEST_INTERVAL_SEC = 1;
+const ERROR = { limit: { code: 429, message: 'Too many requests' } };
+
+let requestCounter = 0;
+let lastResetTime = Date.now();
+
 router.get('/api', (req, res) => {
+  const timeNow = Date.now();
+  if (timeNow - lastResetTime >= REQUEST_INTERVAL_SEC * 1000) {
+    requestCounter = 0;
+    lastResetTime = timeNow;
+  }
+
+  requestCounter++;
+  if (requestCounter > REQUEST_LIMIT) {
+    res.status(ERROR.limit.code).json({ error: ERROR.limit.message });
+    return;
+  }
+
   const randomDelay = Math.floor(Math.random() * 1000) + 1;
+  const index = req.query.index;
   setTimeout(() => {
-    res.json({ index: req.query.index });
+    res.json({ index: index });
   }, randomDelay);
 });
 
