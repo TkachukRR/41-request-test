be7136732fbd3ebed8bf0ad2208536daae9926dd
Roman Tkachuk <tkachukrr@gmail.com>
2024-02-29 14:34:59 +0100

    Added random response delay 1-1000ms

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/api', (req, res) => {
-  res.json({ index: req.query.index });
+  const randomDelay = Math.floor(Math.random() * 1000) + 1;
+  setTimeout(() => {
+    res.json({ index: req.query.index });
+  }, randomDelay);
 });
 
 module.exports = router;
